0af701b2934cfdfb974145ca0e9b1ea3bce03d0f
Leo <gioleo999@gmail.com>
2025-12-22 17:51:37 +0100

    feat(frontend): add plants API

--- frontend/api.py
+++ frontend/api.py
@@ -2,16 +2,62 @@ import requests
 
 BASE_URL = "http://127.0.0.1:5000"  # Flask backend
 
-def get_predictions():
+def get_plants():
     """
-    Fetch predictions from the Flask API.
+    Fetch plants from the Flask API.
+
     Returns:
-        dict: {"real": [...], "predicted": [...]}
+        list: [
+            {"id": "plant_id", "name": "plant_name"},
+            ...
+        ]
     """
-    try:
-        response = requests.get(f"{BASE_URL}/predictions")
-        response.raise_for_status()  # raise error if status != 200
+    try: 
+        response = requests.get(f"{BASE_URL}/plants")
+        response.raise_for_status()
         return response.json()
     except requests.RequestException as e:
-        print(f"Error fetching predictions: {e}")
-        return {"real": [], "predicted": []}
+        print(f"Error fetching plants: {e}")
+        return 
+    
+def get_predictions_by_plant_id(plant_id):
+    """
+    Fetch predictions for a specific plant from the Flask API.
+
+    Args:
+        plant_id (str): ID of the plant
+
+    Returns:
+        list: [
+            {"timestamp": ISO8601 string, "power": float},
+            ...
+        ]
+    """
+    try:
+        response = requests.get(f"{BASE_URL}/plants/{plant_id}/predictions")
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error fetching predictions for plant {plant_id}: {e}")
+        return []
+    
+def get_measurements_by_plant_id(plant_id):
+    """
+    Fetch measurements for a specific plant from the Flask API.
+
+    Args:
+        plant_id (str): ID of the plant
+
+    Returns:
+        list: [
+            {"timestamp": ISO8601 string, "power": float},
+            ...
+        ]
+    """
+    try:
+        response = requests.get(f"{BASE_URL}/plants/{plant_id}/measurements")
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error fetching predictions for plant {plant_id}: {e}")
+        return []
